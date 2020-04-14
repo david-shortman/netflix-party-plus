f0c93a4b225cc1586fb51013377a12d44b90ca64
David <davidmccoy@mailfence.com>
2020-04-14 00:11:15 -0400

    fixed spelling of UpdateSessionMessage type

--- flutter_app/lib/messages/update-session/UpdateSessionMessage.dart
+++ flutter_app/lib/messages/update-session/UpdateSessionMessage.dart
@@ -3,7 +3,7 @@ import 'package:flutterapp/messages/update-session/UpdateSessionContent.dart';
 import '../Message.dart';
 
 class UpdateSessionMessage extends SocketMessage {
-  String type = 'udpateSession';
+  String type = 'updateSession';
 
   UpdateSessionMessage(UpdateSessionContent content) {
     super.content = content;
