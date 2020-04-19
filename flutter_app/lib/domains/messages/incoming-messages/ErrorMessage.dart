fafbc68b8fb215a3dbab23f238362370566273d4
jrlambs <jrlambs@gmail.com>
2020-04-19 00:16:18 -0400

    show toast on connect failure (#52)
    * show toast on connect failure
    
    * blah
    
    * use e
    
    Co-authored-by: Gogs <gogs@fake.local>

---
+++ flutter_app/lib/domains/messages/incoming-messages/ErrorMessage.dart
@@ -0,0 +1,10 @@
+import 'ReceivedMessage.dart';
+
+class ErrorMessage extends ReceivedMessage {
+  String errorMessage;
+
+  ErrorMessage(Map<String, dynamic> objectFromMessage) {
+    errorMessage = objectFromMessage['errorMessage'] as String;
+  }
+
+}
