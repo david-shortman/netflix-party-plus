9390a00468f2c1a1c51be684ff2c3f096697941d
david-shortman <david-shortman@users.noreply.github.com>
2020-04-20 03:34:23 +0000

    Apply Auto Formatter

--- flutter_app/lib/pages/MainPage.dart
+++ flutter_app/lib/pages/MainPage.dart
@@ -130,8 +130,7 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
           ),
           IconButton(
-            icon: SvgPicture.asset(
-                _icon != null ? 'assets/avatars/$_icon' : '',
+            icon: SvgPicture.asset(_icon != null ? 'assets/avatars/$_icon' : '',
                 height: 85),
             onPressed: () {
               goToAccountSettings(context);
