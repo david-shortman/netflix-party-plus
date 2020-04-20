277efdfd72500b49972e09c27dd90d96bf4822e6
David <davidmccoy@mailfence.com>
2020-04-19 23:51:44 -0400

    Update ChatStream.dart

--- flutter_app/lib/widgets/ChatStream.dart
+++ flutter_app/lib/widgets/ChatStream.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutterapp/domains/messages/outgoing-messages/join-session/UserSettings.dart';
+import 'package:flutterapp/domains/messages/outgoing-messages/typing/TypingContent.dart';
+import 'package:flutterapp/domains/messages/outgoing-messages/typing/TypingMessage.dart';
+import 'package:flutterapp/domains/messenger/Messenger.dart';
 import 'package:flutterapp/theming/AvatarColors.dart';
 
 class ChatStream {
