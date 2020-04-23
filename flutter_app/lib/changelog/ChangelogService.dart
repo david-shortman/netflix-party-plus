cafc0720a6fff4ecbed2844e63bbfb07503b7f8e
David <davidmccoy@mailfence.com>
2020-04-23 02:46:19 -0400

    Update ChangelogService.dart (#88)

--- flutter_app/lib/changelog/ChangelogService.dart
+++ flutter_app/lib/changelog/ChangelogService.dart
@@ -1,12 +1,11 @@
 class ChangelogService {
   static String getLatestVersion() {
-    return '0.1.2';
+    return '1.0.0';
   }
 
   static String getCurrentChangelog() {
-    return '''- Added sliding panel for media controls
-- Added indictor that someone is typing
-- Colored message bubbles for each partier
-- Some other design tweaks''';
+    return '''- Welcome to NP+!
+- Auto-reconnects to chat when app is opened
+- Squashes some bugs''';
   }
 }
