3dbde2a3ed7aa6dc5e8730131c16dc6603496add
David <davidmccoy@mailfence.com>
2020-04-26 20:07:59 -0400

    Update ChangelogService.dart

--- flutter_app/lib/changelog/ChangelogService.dart
+++ flutter_app/lib/changelog/ChangelogService.dart
@@ -1,11 +1,11 @@
 class ChangelogService {
   static String getLatestVersion() {
-    return '1.1.0';
+    return '1.2.0';
   }
 
   static String getCurrentChangelog() {
-    return '''- Long pressing on chat avatar icons changes them to use initials instead
-- Play/pause stability fixed
+    return '''- Added skip 10 seconds forward/backward buttons
+- Long pressing on chat avatar icons changes them to use initials instead
 - Added haptic feedback''';
   }
 }
